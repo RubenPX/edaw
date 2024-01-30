d6b1418167d342cd50f23cf61bbca9e6f9a41cd9
RubenPX <rubenmarchante@gmail.com>
2024-01-30 13:45:35 +0100

    maybe it works on build??

--- dist/Event/EventError.js
+++
@@ -1,7 +0,0 @@
-export class EventError extends Error {
-    eventMessage;
-    constructor(msgEvent) {
-        super(typeof msgEvent.returnData.message === 'string' ? msgEvent.returnData.message : 'custom error');
-        this.eventMessage = msgEvent;
-    }
-}
